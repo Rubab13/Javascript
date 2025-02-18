7b326497895df323ea27530a6669bdd537b815ce
Rubab <ahmadrubab13@gmail.com>
2025-02-19 02:22:04 +0500

    Remove Duplicates from an Array

---
+++ removeDuplicates.js
@@ -0,0 +1,16 @@
+// Remove Duplicates from an Array
+
+function removeDuplicates(arr) {
+  let array = arr;
+  let newArray = [];
+
+  for (let i=0; i < array.length; i++) {
+    if (!newArray.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
