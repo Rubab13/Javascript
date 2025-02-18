a09a057736387353690d4ed34af12ce928d03be5
Rubab <ahmadrubab13@gmail.com>
2025-02-19 02:01:08 +0500

    Initial Commit - Javascript questions

---
+++ findLargest.js
@@ -0,0 +1,10 @@
+let array = [1, 2, 3, 4, 5, 6, 7]
+let max = array[0];
+
+for (let i = 0; array.length; i++) {
+  if (array[i] > max) {
+    max = array[i];
+  }
+}
+
+console.log("The largest number in the given array is " + max + ".")
