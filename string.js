1f606d9be8111b9afd5b34ee5ac8d0aa6b640fe2
Rubab <ahmadrubab13@gmail.com>
2025-02-20 02:30:29 +0500

    String Manipulation

---
+++ string.js
@@ -0,0 +1,9 @@
+// A function that converts a string to upper case and lower case.
+
+function Convert (string) {
+  return `Given String: ${string} \nUppercase version: ${string.toUpperCase()} \nLowercase version: ${string.toLowerCase()}\n`
+}
+
+console.log(Convert("rubab"));
+console.log(Convert("Hello"));
+console.log(Convert("likewise"));
