9424d1f50aad60e020c7978852d70ed3d66de93e
Rubab <ahmadrubab13@gmail.com>
2025-02-20 02:24:54 +0500

    absolute value of a given number

---
+++ absolute.js
@@ -0,0 +1,8 @@
+// function that returns the absolute value of a given number
+
+function absolute (number) {
+  return number>0 ? number : -(number);
+}
+
+console.log(absolute(5));
+console.log(absolute(-5));
