323065ffc593089499e896a5fb2d3fb921c0139d
Rubab <ahmadrubab13@gmail.com>
2025-02-20 02:19:44 +0500

    Convert Fahrenheit to Celsius

---
+++ temperatureConversion.js
@@ -0,0 +1,11 @@
+// Convert Fahrenheit to Celsius
+
+function Convert (temp) {
+  return ((temp - 32 ) *5) / 9;
+}
+
+console.log(Convert(32));
+console.log(Convert(33));
+console.log(Convert(0));
+console.log(Convert(10));
+console.log(Convert(74));
