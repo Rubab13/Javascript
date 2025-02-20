3c469b44df12a62c474af88f323bea0acfd8c01b
Rubab <ahmadrubab13@gmail.com>
2025-02-20 11:13:37 +0500

    positive negative checker

---
+++ positiveNegativeChecker.js
@@ -0,0 +1,10 @@
+// A function that checks whether a number is positive, negative or zero.
+
+function checkNumber (number) {
+  number>=0 ? console.log(`The number ${number} is a positive number.`) : console.log(`The number ${number} is a negative number.`);
+}
+
+checkNumber(8);
+checkNumber(0);
+checkNumber(-1);
+checkNumber(-12987234);
