4c1f83e8986a65b4d8f590decbfed04b4a9ea9ea
Rubab <ahmadrubab13@gmail.com>
2025-02-27 12:27:18 +0500

    String Combinations

---
+++ stringCombinations.js
@@ -0,0 +1,13 @@
+// Write a JavaScript function that generates all combinations of a string.
+// Example string : 'dog'
+// Expected Output : d,do,dog,o,og,g
+
+// Do this through recursion
+function stringCombinations (string) {
+
+
+
+  return [];
+}
+
+console.log(stringCombinations("dog"));
